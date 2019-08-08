c0ef6cfab6e2117c22f5f5a281f066439b91a217
abhivarshu <chinnivalluri@gmail.com>
2019-08-07 22:49:09 -0500

    applying function setter

---
+++ js/Setter.js
@@ -0,0 +1,11 @@
+var language = {
+  set current(name) {
+    this.log.push(name);
+  },
+  log: []
+}
+
+language.current = 'EN';
+language.current = 'FA';
+
+console.log(language.log);
---
+++ js/apply.js
@@ -0,0 +1,10 @@
+var numbers = [5, 6, 2, 3, 7];
+
+var max = Math.max.apply(null, numbers);
+
+console.log(max);
+// expected output: 7
+
+var min = Math.min.apply(null, numbers);
+
+console.log(min);
